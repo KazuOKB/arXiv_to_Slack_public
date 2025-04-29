e193ddab3a722f5f0e8618e9b93b3888a6dcc7fc
OKB <bayashioka@gmail.com>
2025-04-29 16:38:40 +0900

    cronデーモン用のlogを追加

--- run_arxiv.sh
+++ run_arxiv.sh
@@ -8,4 +8,5 @@
 cd "$(dirname "$0")"
 
 # (2) conda run で環境"api"のPythonを呼び出しスクリプトを実行
-conda run -n api python3 my_arXiv_slackbot.py
+# activate/source は一切せずに、conda run だけ使う
+/opt/anaconda3/bin/conda run -n api python3 my_arXiv_slackbot.py
